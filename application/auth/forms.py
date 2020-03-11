285317fec0d584378371131f8a5679c8564f3576
Chenhui Zhao <32270417+lchz@users.noreply.github.com>
2020-03-11 15:58:27 +0200

    Added sign-up form in auth/forms.py

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -8,3 +8,11 @@ class LoginForm(FlaskForm):
 
     class Meta:
         csrf = False
+
+class SignupForm(FlaskForm):
+    name = StringField("Name:", [validators.length(min=3), validators.required()])
+    username = StringField("Username:", [validators.unique(), validators.required(), validators.length(min=3)])
+    password = PasswordField("Password:", [validators.required(), validators.length(min=3)])
+    
+    class Meta:
+        csrf = False
