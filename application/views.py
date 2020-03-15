8ea645e8a38086e29caab17ae1e1c6920a4ce148
chenhuiz <chenhui.zhao@helsinki.fi>
2020-03-15 16:55:03 +0200

    Added 'My page' to check registered courses

--- application/views.py
+++ application/views.py
@@ -1,6 +1,18 @@
 from flask import render_template
 from application import app
 
+from flask_login import login_required, current_user
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/myPage')
+@login_required
+def myPage():
+
+    projects = current_user.registrations
+
+    return render_template('myPage.html', projects=projects)
