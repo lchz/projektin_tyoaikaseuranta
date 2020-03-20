1f13dc7261e99fe23a310cb90246467ffefe549b
chenhuiz <chenhui.zhao@helsinki.fi>
2020-03-20 10:26:09 +0200

    Forms bootstrap

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -3,14 +3,14 @@ from wtforms import PasswordField, StringField, validators
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Username:")
-    password = PasswordField("Password:")
+    username = StringField("name", [validators.required()])
+    password = PasswordField("password", [validators.required()])
 
     class Meta:
         csrf = False
 
 class SignupForm(FlaskForm):
-    name = StringField("Name:", [validators.required(), validators.length(min=3),])
+    name = StringField("Name:", [validators.required(), validators.length(min=3)])
     username = StringField("Username:", [validators.required(), validators.length(min=3)])
     password = PasswordField("Password:", [validators.required(), validators.length(min=3)])
     
